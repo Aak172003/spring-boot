149ef62e14a70077751bdecaeda41b07a3d4fa4c
Aakashmoondive502 <aakash.prajapati@moondive.co>
2025-07-23 11:16:08 +0530

    implement-data-transfer-object-as-userrequest-and-userresponse

---
+++ eshop-application/src/main/java/com/app/eshop/dto/UserResponse.java
@@ -0,0 +1,18 @@
+package com.app.eshop.dto;
+
+import com.app.eshop.models.UserRole;
+import lombok.Data;
+
+@Data
+public class UserResponse {
+    private String id;
+    private String firstName;
+    private  String lastName ;
+    private String email;
+    private String phoneNo;
+    //    This is default role is CUSTOMER ,
+    //    But if i provide role from postman it will assign as i pass ADMIN so it will assign
+    private UserRole role ;
+
+    private AddressDTO address;
+}
---
+++ eshop-application/src/main/java/com/app/eshop/models/Product.java
@@ -0,0 +1,4 @@
+package com.app.eshop.models;
+
+public class Product {
+}
